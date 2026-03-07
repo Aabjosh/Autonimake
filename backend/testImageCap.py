64652738bd1f0394c84906a93dc9d02ac0deecf1
welthyheyy <bdkurseja@gmail.com>
2026-03-07 04:43:49 -0500

    improved visual changes

--- backend/testImageCap.py
+++ backend/testImageCap.py
@@ -7,6 +7,16 @@ frame_count = 0
 saved_count = 0
 save_interval = 5
 
+ret,frame = cap.read()
+height, width, _ = frame.shape
+box_height = 600
+box_width = 600
+
+x1 = (width // 2) - (box_width // 2)
+y1 = (height // 2) - (box_height // 2)
+x2 = (width // 2) + (box_width // 2)
+y2 = (height // 2) + (box_height // 2)
+
 
 folder = 'pytorch_dataset'
 if not os.path.exists(folder):
@@ -19,20 +29,27 @@ if not cap.isOpened():
 print("Press 'q' to quit")
 
 
-
 while True:
     ret, frame = cap.read()
+
 
     if not ret:
         print("Error: Failed to read frame.")
         break
 
-    cv2.imshow('frame',frame)
+    cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 0, 255), 2)
+    roi = frame[y1:y2, x1:x2]
+    dimmed_frame = (frame * 0.5).astype('uint8')
+    dimmed_frame[y1:y2, x1:x2] = frame[y1:y2, x1:x2]
+
+    cv2.imshow('frame', dimmed_frame)
 
     if frame_count % save_interval == 0:
         filename = os.path.join(folder, f"frame_{frame_count:04d}.jpg")
-        cv2.imwrite(filename,frame)
+        cv2.imwrite(filename,roi)
         saved_count += 1
+
+
 
     frame_count +=1
 
